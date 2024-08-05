fc0e20f917b241222bb5bcfd12654e759deb9992
Riyanda Azis Febrian <riyandaazis00@gmail.com>
2024-08-05 13:48:44 +0700

    Updated Login Logic

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['middleware' => 'cors'], function () {
+    Route::get('login', [AuthController::class, 'auth']);
+    Route::get('public-post-list', [PostController::class, 'postList']);
+});
+
