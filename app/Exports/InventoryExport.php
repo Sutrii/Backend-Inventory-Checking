d6e79279392e7e212496d4f9913f71d97c586499
Riyanda Azis Febrian <riyandaazis00@gmail.com>
2024-08-26 23:17:23 +0700

    Removed Gambar from Export Excel

--- app/Exports/InventoryExport.php
+++ app/Exports/InventoryExport.php
@@ -3,6 +3,7 @@
 namespace App\Exports;
 
 use App\Models\InventoryItem;
+use Illuminate\Support\Facades\Log; // Import Log facade
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 
@@ -15,29 +16,35 @@ class InventoryExport implements FromCollection, WithHeadings
      */
     public function collection()
     {
-        return InventoryItem::all()->map(function ($item) {
-            return [
-                'Kategori Barang' => $item->kategori_input,
-                'Nama Barang' => $item->nama_barang,
-                'Tipe Barang' => $item->tipe_barang,
-                'Kualitas' => $item->kualitas,
-                'Tanggal' => $item->tanggal,
-                'Tanggal Awal Peminjaman' => $item->tanggal_awal_pinjam,
-                'Tanggal Akhir Peminjaman' => $item->tanggal_akhir_pinjam,
-                'Nama Peminjam' => $item->nama_peminjam,
-                'Divisi Peminjam' => $item->divisi_peminjam,
-                'Serial Number' => $item->sn,
-                'Jumlah' => $item->jumlah,
-                'Satuan' => $item->satuan,
-                'Gambar' => $item->picture,
-                'Keterangan' => $item->keterangan,
-                'Unit Kerja' => $item->unitKerja,
-                'Lokasi' => $item->lokasi,
-                'Status Barang' => $item->status_barang,
-                'Solusi Barang' => $item->solusi_barang,
-                'Tujuan Keluar' => $item->tujuan_keluar,
-            ];
-        });
+        try {
+            $items = InventoryItem::all();
+            $mappedItems = $items->map(function ($item) {
+                return [
+                    'Kategori Barang' => $item->kategori_input,
+                    'Nama Barang' => $item->nama_barang,
+                    'Tipe Barang' => $item->tipe_barang,
+                    'Kualitas' => $item->kualitas,
+                    'Tanggal' => $item->tanggal,
+                    'Tanggal Awal Peminjaman' => $item->tanggal_awal_pinjam,
+                    'Tanggal Akhir Peminjaman' => $item->tanggal_akhir_pinjam,
+                    'Nama Peminjam' => $item->nama_peminjam,
+                    'Divisi Peminjam' => $item->divisi_peminjam,
+                    'Serial Number' => $item->sn,
+                    'Jumlah' => $item->jumlah,
+                    'Satuan' => $item->satuan,
+                    'Keterangan' => $item->keterangan,
+                    'Unit Kerja' => $item->unitKerja,
+                    'Lokasi' => $item->lokasi,
+                    'Status Barang' => $item->status_barang,
+                    'Solusi Barang' => $item->solusi_barang,
+                    'Tujuan Keluar' => $item->tujuan_keluar,
+                ];
+            });
+            return $mappedItems;
+        } catch (\Exception $e) {
+            Log::error('Terjadi error saat melakukan export inventory items: ' . $e->getMessage());
+            throw $e;
+        }
     }
 
     /**
@@ -60,7 +67,6 @@ class InventoryExport implements FromCollection, WithHeadings
             'Serial Number',
             'Jumlah',
             'Satuan',
-            'Gambar',
             'Keterangan',
             'Unit Kerja',
             'Lokasi',
@@ -70,4 +76,5 @@ class InventoryExport implements FromCollection, WithHeadings
         ];
     }
 }
+
 
